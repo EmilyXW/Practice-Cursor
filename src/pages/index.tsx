ee0d286d51c90d9c19cb600630f5e6b6f148d5f2
EmilyXW <xuyouwang10@gmail.com>
2025-07-02 22:36:33 -0400

    Fix build issues

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,39 @@
+import { Carousel, CarouselItem, CarouselContent } from "@/components/ui/carousel";
+
 export default function Home() {
-  return <h1>Hello World</h1>;
+  return (
+    <div>
+      <Carousel>
+        <CarouselContent>
+          <CarouselItem>
+            <img
+              src="/clouds.jpeg"
+              alt="cloud and koi unfinished painting"
+              style={{
+                width: "100%",
+                height: 300,
+                objectFit: "cover",
+                display: "block",
+              }}
+            />
+          </CarouselItem>
+          <CarouselItem>
+            <img
+              src="/catnipMouse.jpeg"
+              alt="Nellie with catnip mouse"
+              style={{
+                width: "100%",
+                height: 300,
+                objectFit: "cover",
+                display: "block",
+              }}
+            />
+          </CarouselItem>
+        </CarouselContent>
+      </Carousel>
+      <h1 style={{ textAlign: 'center', fontSize: 100, marginTop: 32, fontFamily: 'Shanfe, cursive' }}>
+        Nellie
+      </h1>
+    </div>
+  );
 }
